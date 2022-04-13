234b777b3b3cd6fdd8b2a9bbb32f51c49c5b9b66
renatodex@gmail.com <renatodex@gmail.com>
2022-04-13 18:57:58 -0300

    First commit

---
+++ pages/components/pie_chart.jsx
@@ -0,0 +1,12 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export default function PieChartComponent ({ data }) {
+  return (
+    <Pie
+      data={data}
+    />
+  )
+}
